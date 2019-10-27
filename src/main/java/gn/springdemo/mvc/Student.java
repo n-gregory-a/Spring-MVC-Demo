b1a111ccad639a8dda4e8ca5f7da713bd46d2e20
n-gregory-a <ovonely@gmail.com>
2019-10-27 20:09:05 +0700

    Spring MVC - Form Tags and Data Binding: create java coded option list for drop-list

--- src/main/java/gn/springdemo/mvc/Student.java
+++ src/main/java/gn/springdemo/mvc/Student.java
@@ -1,10 +1,27 @@
 package gn.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private  String lastName;
     private String country;
+
+    private LinkedHashMap<String, String> countryOptions;
+
+    public Student() {
+
+        //populate country options: used ISO country code
+        countryOptions = new LinkedHashMap<String, String>();
+
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("US", "United States of America");
+
+    }
 
     public String getFirstName() {
         return firstName;
@@ -28,5 +45,9 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
     }
 }
